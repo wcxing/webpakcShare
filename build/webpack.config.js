c5f9ec00c3f039e475e83ed39a68725ebbab7f3a
wuchangxing <cx15010215661@163.com>
2018-07-23 07:24:48 +0800

    step two 开发优化，配置devserver、热更新、happypack

--- build/webpack.config.js
+++ build/webpack.config.js
@@ -1,17 +1,25 @@
 const path = require('path')
+const webpack = require('webpack')
+const HtmlWebpackPlugin = require('html-webpack-plugin')
+
+// happypack
+const HappyPack = require('happypack')
+const os = require('os')
+const HappyThreadPool = HappyPack.ThreadPool({ size: os.cpus().length }) //启动线程池
 
 module.exports = {
     entry: ['babel-polyfill', './src/test.js'],
-    output: {
-        path: path.resolve(__dirname, '../output'),
-        publicPath: '../output/'
-    },
+    // output: {
+    //     path: path.resolve(__dirname, '../output'),
+    //     publicPath: '../output/'
+    // },
     module: {
         rules: [
-        { 
+        {
             test: /\.js$/,
             exclude: /node_modules/,
-            loader: 'babel-loader'
+            // loader: 'babel-loader'
+            loader: 'happypack/loader?id=babel'
         },
         {
             test: /\.css$/,
@@ -38,6 +46,34 @@ module.exports = {
             test: /\.(woff|svg|woff2|eot|ttf|otf)$/,
             loader: 'url-loader'
         }
+        ]
+    },
+    devServer: {
+        host: '0.0.0.0',
+        port: '3333',
+        open: true,
+        // publicPath: '/',
+        contentBase: path.resolve(__dirname, '../src'),
+        historyApiFallback: true,
+        hot: true
+    },
+    plugins: [
+        new HtmlWebpackPlugin({
+            template: path.join(__dirname, '../src/index.html')
+        }),
+        new webpack.HotModuleReplacementPlugin(),
+        // happypack
+        new HappyPack({
+            // 用唯一的标识符 id 来代表当前的 HappyPack 是用来处理一类特定的文件
+            id: 'babel',
+            // 如何处理 .js 文件，用法和 Loader 配置中一样
+            loaders: [
+            {
+                loader: 'babel-loader'
+            }
+            ],
+            threadPool: HappyThreadPool
+            // ... 其它配置项
+        })
     ]
-    } 
 }

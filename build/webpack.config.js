bef3b7af86661e4602d3a0c30c42faccbbc7654b
wuchangxing <cx15010215661@163.com>
2018-07-22 22:41:23 +0800

    第一步，基础配置

--- build/webpack.config.js
+++ build/webpack.config.js
@@ -1,16 +1,42 @@
 const path = require('path')
 
 module.exports = {
-    enrty: '../src/test.js',
-    // output: {
-    //     pathname: 
-    // }
+    entry: ['babel-polyfill', './src/test.js'],
+    output: {
+        path: path.resolve(__dirname, '../output'),
+        publicPath: '../output/'
+    },
     module: {
         rules: [
+        { 
+            test: /\.js$/,
+            exclude: /node_modules/,
+            loader: 'babel-loader'
+        },
         {
             test: /\.css$/,
             exclude: /node_modules/,
             loaders: ['style-loader', 'css-loader']
+        },
+        {
+            test: /\.less$/,
+            exclude: /node_modules/,
+            loaders: ['style-loader', 'css-loader', 'less-loader']
+        },
+        {
+            test: /\.(png|jpg|gif)$/,
+            use: [
+            {
+                loader: 'url-loader',
+                options: {
+                limit: 8192
+                }
+            }
+            ]
+        },
+        {
+            test: /\.(woff|svg|woff2|eot|ttf|otf)$/,
+            loader: 'url-loader'
         }
     ]
     } 
--- src/test.js
+++ src/test.js
@@ -1,9 +1,34 @@
 // const _ = require('lodash')
 import _ from 'lodash'
-import 'style-loader!css-loader!./style.css'
+import './style.css'
+import './style.less'
+import './assest/font/icomoon/style.css'
+import img from './class_01.jpg'
+
+function loadImg(url) {
+    var img = document.createElement('img')
+    img.src = url
+    document.body.appendChild(img)
+}
 
 function print(data) {
     console.log(data)
 }
 
+const delay = async () => {
+    const value = await getRequist()
+    console.log(value)
+}
+
+function getRequist() {
+    return new Promise((resolve, reject) => {
+        setTimeout(() => {
+            const value = 'new vlaue!'
+            resolve(value)
+        }, 2000)
+    } )
+}
 print(123)
+loadImg(img)
+delay()
+console.log(111)

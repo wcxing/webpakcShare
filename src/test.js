a48765917a0cfce88357c6c00367421f1f2ee91c
wuchangxing <cx15010215661@163.com>
2018-07-23 09:53:35 +0800

    react 配置end

--- src/test.js
+++ src/test.js
@@ -4,6 +4,8 @@ import './style.css'
 import './style.less'
 import './assest/font/icomoon/style.css'
 import img from './class_01.jpg'
+import React from 'react'
+import ReactDOM from 'react-dom'
 
 function loadImg(url) {
     var img = document.createElement('img')
@@ -32,3 +34,26 @@ print(123)
 loadImg(img)
 delay()
 console.log(111)
+
+const rootElement = document.getElementById('app')
+
+class TestComponent extends React.Component {
+    constructor(props) {
+        super(props)
+        this.state = {
+            count: 10
+        }
+    }
+    // 生命周期函数
+    componentWillMount() {
+    }
+    render() {
+        return (
+            <div>
+                <h3>测试react！</h3>
+                <div>当前数量：{this.state.count}</div>
+            </div>
+        )
+    }
+}
+ReactDOM.render(<TestComponent />, rootElement)

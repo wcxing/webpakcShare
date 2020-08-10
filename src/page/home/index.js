d9959f56b33a370aec53e0fae7a8be0a6c07ee74
wucx <cx15010215661@163.com>
2020-08-10 14:52:21 +0800

    xgPlayer

--- src/page/home/index.js
+++ src/page/home/index.js
@@ -1,52 +1,50 @@
 import React, { PureComponent } from 'react';
-import Tooltip from 'wenzai_ui/lib/tooltip';
-// const { Tooltip } = require('wenzai_ui');
+import 'xgplayer';
+import FlvJsPlayer from 'xgplayer-flv.js';
 
-console.log(11111, Tooltip)
+// import Player from 'xgplayer';
+// import 'xgplayer-mp4';
+
 // import _ from 'lodash'
+
+// import adiao from '../../video/adiao.mp4';
 class Home extends PureComponent {
   constructor(props) {
     super(props)
     this.state = {
-      count: 20
     }
-    console.log('constructor', props)
   }
-  // componentWillMount() {
-  //     this._getCamelCase('my-camelcase-name')
-  //     console.log('I am login page, componentwillmount!')
-  // }
 
-  // shouldComponentUpdate(nextProps, nextState) {
-  //   console.log('home shouldComponentUpdate')
-  //   console.log('nextProps', nextProps)
-  //   console.log('this.props', this.props)
-  //   console.log('nextState', nextState)
-  //   console.log('this.state', this.state)
-  //   return false
-  // }
-  componentWillReceiveProps(nextProps) {
-    console.log('componentWillReceiveProps_nextProps', nextProps)
-    console.log('componentWillReceiveProps_thisProps', this.props)
+  componentDidMount() {
+    const player = new FlvJsPlayer({
+      id: 'xg',
+      url: 'https://pullws-live.genshuixue.com/mgclient/6007010277945481-2489960001.flv',
+      // url: 'http://static.fxuetang.speiyou.com/1517366097463_6902252.mp4',
+      // poster: './poster.png',
+      isLive: true,
+      preloadTime: 30,
+      minCachedTime: 5,
+      cors: true,
+      flvOptionalConfig: {
+        // fixAudioTimestampGap: false,
+        enableWorker: true,
+        // stashInitialSize: 500,
+        // autoCleanupSourceBuffer: true,
+        // enableWorker: true,
+        // enableStashBuffer: false,
+        // stashInitialSize: 120,
+      },
+    });
+
+    // player.on();
+
   }
-  _setState() {
-    this.setState({
-      count: 222
-    })
-  }
+
   render() {
-    console.log('home render');
     return (
-      <div>
-        <h2> this is HomePage! </h2>
-        <div className="img-test-1">test css!</div>
-        <button onClick={ () => { this._setState() } }>son setState</button>
-        <div>
-          <h3>子组件count{this.state.count}</h3>
-          <Tooltip content="文字提示" style={{marginRight:"20px"}}>
-            <button>上边文字提示</button>
-          </Tooltip>
-        </div>
+      <div id="xg">
+        {/* <h2> this is HomePage! </h2>
+        <div className="img-test-1">test css!</div> */}
       </div>
     )
   }
